a9711ee8ad49e973ef39b25f852b5ec8b7198e80
gofffro <vazheninstepan@gmail.com>
2025-05-02 21:46:48 +0700

    Реализовано логирование синхронизации в формате XML

---
+++ Models/XmlLogger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace log020525.Models
+{
+  public static class XmlLogger
+  {
+    private static readonly string s_logFileName = "sync_log.xml";
+
+    public static void SaveLog(List<SyncLogEntry> logEntries)
+    {
+      var serializer = new XmlSerializer(typeof(List<SyncLogEntry>));
+      using (var stream = File.Create(s_logFileName))
+      {
+        serializer.Serialize(stream, logEntries);
+      }
+    }
+
+    public static List<SyncLogEntry> LoadLog()
+    {
+      if (!File.Exists(s_logFileName))
+      {
+        return new List<SyncLogEntry>();
+      }
+
+      var serializer = new XmlSerializer(typeof(List<SyncLogEntry>));
+      using (var stream = File.OpenRead(s_logFileName)) 
+      {
+        return (List<SyncLogEntry>)serializer.Deserialize(stream);
+      }
+    }
+  }
+}
